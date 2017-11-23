9d14bed32a2cc0481d98743e9c62554f3efe8f0f
박혜인 <hyein0801@gmail.com>
2017-11-24 00:50:59 +0900

    portfolio

---
+++ Toryburch/footer.php
@@ -0,0 +1,41 @@
+        <!-- TB_main_footer--> 
+         
+        <footer class="global_footer">
+            <div class="footer_section_bg">
+                <div class="footer_head">
+                    <img src="images/tory-logo.png" class="footer_logo">
+                    <div class="social_ico" clearfix>
+                        <a href="#"><img src="images/facebook.png"></a>
+                        <a href="#"><img src="images/twiiter.png"></a>
+                        <a href="#"><img src="images/pinterest.png"></a>
+                        <a href="#"><img src="images/youtube.png"></a>
+                    </div>
+                    <img src="images/footer_line.png" class="footer_line">
+                </div>
+                <div class="footer_body" clearfix>
+                    <div class="company_box">
+                        <a href="http://www.toryburch.co.kr"><img src="images/company_ico.png"></a>
+                        <h4>COMPANY</h4>
+                        <p>대표 : 최지훈<br>사업자 번호 : 101-85-43600</p>
+                    </div>
+                    <div class="call_box">
+                        <a href="tel:02-1599-9679"><img src="images/phone_ico.png"></a>
+                        <h4>CALL</h4>
+                        <p>고객센터 : 1599-9679<br>FAX : 02-2266-1202</p>
+                    </div>
+                    <div class="focus_box">
+                        <a href="#"><img src="images/map_ico.png"></a>
+                        <h4>FOCUS ON</h4>
+                        <p>서울특별시 강남구 도곡동<br>467-12 삼성물산(주)패션부문</p>                    
+                    </div>
+                </div>
+            </div>
+            <div class="copyright_wrap">
+                <a href="#">
+                    <p class="copyright">COPYRIGHT 2017. PARK HYEIN ALL RIGHTS RESERVED.</p>
+                </a>
+            </div>
+        </footer>
+        
+    </body>
+</html>
---
+++ Toryburch/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>TORY BURCH</title>
+        <link rel="stylesheet" href="grid.css">        
+        <link rel="stylesheet" href="TB_main_style.css">
+        <link rel="stylesheet" href="TB_sub_style.css">        
+        <link rel="stylesheet" href="TB_mobile.css">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109647473-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-109647473-1');
+        </script>
+    </head>
+    
+    <body>
+
+        <!-- TB_main_header -->
+        <header class="global_header">
+            <nav class="global_nav clearfix">
+                <a href class="logo">
+                    <img src="./images/TB_main_logo_size.png">
+                </a>
+                <ul class="nav_lst clearfix">
+                    <li><a href="#">CATEGORIES</a></li>
+                    <li><a href="#">COLLECTIONS</a></li>
+                    <li><a href="#">STORE</a></li>
+                    <li><a href="#">EVENT</a></li>
+                    <li><a href="#">ORDER</a></li>
+                    <li><a href="#"><img src="images/TB_main_serch_pickto.png" class="serch_picto"></a></li>
+                </ul>
+            </nav>
+        </header>
+        
+        <!-- TB_mobile_nav-->         
+        <div class="mobile_nav_wrap">
+            <div class="mobile_nav clearfix">
+                <a href="#" class="snb">
+                    <img src="images/mobile/side_menu_ico.png">
+                </a>
+                <a href class="mb_logo">
+                    <img src="images/mobile/100x20_logo.png">
+                </a>
+                <a href="#" class="mobile_serch_picto">
+                    <img src="images/mobile/mobile_serch_ico.png">
+                </a>
+            </div>    
+        </div>
