7c9def222935eb72d049229b8a18c5de87007604
박혜인 <hyein0801@gmail.com>
2017-11-23 21:02:09 +0900

    Basic site coding

---
+++ header_snb.php
@@ -0,0 +1,15 @@
+
+        <header class="side_header">
+            <div class="side_nav clearix">
+                <ul class="side_lis">
+                    <a href="coder.html"><li class="lis">Coder</li></a>
+                    <a href="coding.html"><li class="lis">Coding</li></a>
+                    <a href="https://github.com/hyein0801/hyeinhub" target="_blank"><li class="lis">Github</li></a>
+                    <a href="#"><li class="lis">SNS</li></a>                   
+                </ul>
+                <div class="copy_name">
+                    <span>2017 Park hyein Portfolio site : Designed by hyein :
+                    hyeinpark0801@gmail.com</span>
+                </div>    
+            </div>    
+        </header>
